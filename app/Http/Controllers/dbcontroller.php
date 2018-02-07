c62071fa24c36e284a576102a96f74cfdb7d7eac
Jet Matias <jbm@datafactorycorp.com>
2018-02-07 11:55:01 +0800

    add export module

--- app/Http/Controllers/dbcontroller.php
+++ app/Http/Controllers/dbcontroller.php
@@ -68,7 +68,7 @@ class dbcontroller extends Controller
   }
   public function exporttrending(Request $r){
     $currentdatetime = date('Ymdhis');
-    $this->GetTrending = DB::select('EXEC GetTrending ?, ?, ?, ?, ?, ?, ?',array($r->ProgTitleID,$r->ChannelGroupID,$r->ChannelID,$r->PeriodTypeID,$r->Period,$rPeriodTypeID,$r->Period,$r->Filter));
+    $this->GetProgramePerformance = DB::select('EXEC GetProgramePerformance ?, ?, ?, ?, ?, ?',array($r->ChannelGroupID,$r->ChannelID,$r->PlatFormID,$r->PeriodTypeID,$r->Period,$r->Filter));
     $file = fopen('csv/trending'. $currentdatetime .'.csv', 'w+');
     foreach ($this->GetTrending as $row) {
       fputcsv($file, [$row->ProgDate,$row->PlatFormID,$row->Sum000,$row->SumATV]);
