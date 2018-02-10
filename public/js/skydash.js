4d8ed48255ff8fc9a87f6e22ce93aba232272050
Jet Matias <jbm@datafactorycorp.com>
2018-02-10 14:49:32 +0800

    update export

--- public/js/skydash.js
+++ public/js/skydash.js
@@ -1687,7 +1687,7 @@ $(function(){
             var output = '';
             lilength = data.length
             for(var i in data){
-              output += '<li data-value="'+data[i].ProgrammeTitle+'" data-toggle="tooltip" title="'+ data[i].ProgrammeTitle +'">'+ data[i].ProgrammeTitle +'</li>'
+              output += '<li data-value="'+data[i].ProgrammeTitle+'" class="tooltip">'+ data[i].ProgrammeTitle +'<span class="tooltiptext">'+ data[i].ProgrammeTitle +'</span></li>'
             }
             $('#list').html(output)
             $('#list li').each(function(){
