7f8059f37e7a53b8fc315081b7c0428d59a95872
Jet Matias <jbm@datafactorycorp.com>
2018-02-13 16:06:19 +0800

    update programme ratings

--- app/Http/Controllers/dbcontroller.php
+++ app/Http/Controllers/dbcontroller.php
@@ -48,7 +48,7 @@ class dbcontroller extends Controller
   public function ExpotChannelPerformance(Request $r){
     $currentdatetime = date('Ymdhis');
     $this->GetChannelPerformance = DB::select('EXEC GetChannelPerformance ?, ?, ?, ?',array($r->ChannelGroupID,$r->PeriodTypeID,$r->Period,$r->Filter));
-    $file = fopen('csv/channelperformance'. $currentdatetime .'.xlsx', 'w+');
+    $file = fopen('csv/channelperformance'. $currentdatetime .'.xls', 'w+');
     fputcsv($file, ["Channel name","platform name","Sum000"]);
     foreach ($this->GetChannelPerformance as $row) {
       if($row->ChannelID != -1){
@@ -58,29 +58,29 @@ class dbcontroller extends Controller
       }      
     }
     fclose($file);
-    return response('csv/channelperformance'. $currentdatetime .'.xlsx');
+    return response('csv/channelperformance'. $currentdatetime .'.xls');
   }
 
   public function exportprogramme(Request $r){
     $currentdatetime = date('Ymdhis');
     $this->GetProgramePerformance = DB::select('EXEC GetProgramePerformance ?, ?, ?, ?, ?, ?',array($r->ChannelGroupID,$r->ChannelID,$r->PlatFormID,$r->PeriodTypeID,$r->Period,$r->Filter));
-    $file = fopen('csv/programmeperfomance'. $currentdatetime .'.xlsx', 'w+');
+    $file = fopen('csv/programmeperfomance'. $currentdatetime .'.xls', 'w+');
     fputcsv($file, ["BMI code","Programme title","Count","Sum000"]);
     foreach ($this->GetProgramePerformance as $row) {
       fputcsv($file, [$row->BMICode,$row->ProgrammeTitle,$row->CNT,$row->Sum000]);
     }
     fclose($file);
-    return response('csv/programmeperfomance'. $currentdatetime .'.xlsx');
+    return response('csv/programmeperfomance'. $currentdatetime .'.xls');
   }
   public function exporttrending(Request $r){
     $currentdatetime = date('Ymdhis');
     $this->GetTrending = DB::select('EXEC GetTrending ?, ?, ?, ?, ?, ?, ?',array($r->ProgTitleID,$r->ChannelGroupID,$r->ChannelID,$r->PeriodTypeID,$r->Period,$r->PlatFormID,$r->Filter));
-    $file = fopen('csv/trending'. $currentdatetime .'.xlsx', 'w+');
+    $file = fopen('csv/trending'. $currentdatetime .'.xls', 'w+');
     fputcsv($file, ["Prog date","Sum000"]);
     foreach ($this->GetTrending as $row) {
       fputcsv($file, [$row->ProgDate,$row->Sum000]);
     }
     fclose($file);
-    return response('csv/trending'. $currentdatetime .'.xlsx');
+    return response('csv/trending'. $currentdatetime .'.xls');
   }    
 }
