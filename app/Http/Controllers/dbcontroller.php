44fffe27d24bb998da2df0637fad053ece8ea18f
Jet Matias <jbm@datafactorycorp.com>
2018-02-13 17:44:35 +0800

    update programme ratings\

--- app/Http/Controllers/dbcontroller.php
+++ app/Http/Controllers/dbcontroller.php
@@ -47,27 +47,13 @@ class dbcontroller extends Controller
   }
   public function ExpotChannelPerformance(Request $r){
     $currentdatetime = date('Ymdhis');
-    $this->GetChannelPerformance = DB::select('EXEC GetChannelPerformance ?, ?, ?, ?',array($r->ChannelGroupID,$r->PeriodTypeID,$r->Period,$r->Filter));
-    $array = Array (
-            0 => Array (
-                    0 => "How was the Food?",
-                    1 => 3,
-                    2 => 4 
-            ),
-            1 => Array (
-                    0 => "How was the first party of the semester?",
-                    1 => 2,
-                    2 => 4,
-                    3 => 0 
-            ) 
-    );
-
+    $this->GetChannelPerformance = DB::select('EXEC GetChannelPerformance ?, ?, ?, ?',array($r->ChannelGroupID,$r->PeriodTypeID,$r->Period,$r->Filter));    
     header("Content-Disposition: attachment; filename=\"demo.xls\"");
     header("Content-Type: application/vnd.ms-excel;");
     header("Pragma: no-cache");
     header("Expires: 0");
     $out = fopen("php://output", 'w');
-    foreach ($array as $data)
+    foreach ($this->GetChannelPerformance as $data)
     {
         fputcsv($out, $data,"\t");
     }
