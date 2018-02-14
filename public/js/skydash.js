aa3a9800e938d696ea139b7c9fff2facbd64358d
Jet Matias <jbm@datafactorycorp.com>
2018-02-14 14:59:48 +0800

    update export

--- public/js/skydash.js
+++ public/js/skydash.js
@@ -512,13 +512,19 @@ var navmodule = {
       output += '</tbody></table>';
       $('#channelperformance').scrollTop(0)
       $('#channelperformance').html(output)
-      var $table = $('table#table-channelperformance');
-      $table.floatThead();      
+
       var channeltable = ($('#channelperformance').width() - 280) / Formname.length
       $('#channelperformance td:not(:first-child)').css({
         width : channeltable+'px',
       })      
 
+      var $table = $('table#table-channelperformance');
+      $table.floatThead({
+        responsiveContainer: function($table){
+            return $table.closest('.table-responsive');
+        }
+      });
+      
       $('#table-channelperformance td:not(:first-child)').each(function(){
         $(this).click(function(){
           if($(this).attr("id")!= undefined){
