106df70667cf8ef58388c2ce16a5b30515acc1e4
Jet Matias <jbm@datafactorycorp.com>
2018-02-16 10:21:15 +0800

    update prog performance

--- public/js/skydash.js
+++ public/js/skydash.js
@@ -650,9 +650,9 @@ var navmodule = {
         '</tr>'+
         '<tr>'+
           '<td width="300"><small>Programme Title</small></td>'+
-          '<td id="sort" value="starttime" value><small># linear runs'+ starttime +'</small></td>' +
+          '<td id="sort" value="starttime" value><small>Start time'+ starttime +'</small></td>' +
           '<td><small>Sky 360 BMI</small></td>'+
-          '<td id="sort" value="Count"><small>Count</small>'+ sortCount +'</td>' + 
+          '<td id="sort" value="Count"><small># linear runs</small>'+ sortCount +'</td>' + 
           '<td id="sort" value="000"><small>000</small>'+ sort000 +'</td>'+          
         '</tr></thead><tbody>';
       }else{
