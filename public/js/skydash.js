03579eed5463e17b8231e8f05ea81d0a658ac51a
Jet Matias <jbm@datafactorycorp.com>
2018-02-21 16:54:56 +0800

    update controller

--- public/js/skydash.js
+++ public/js/skydash.js
@@ -235,6 +235,11 @@ var navmodule = {
       expotRows.push([img])
       alasql("SELECT * INTO "+ $("input[name='format']:checked").val() +" ('Trending"+ datetime +"."+ $("input[name='format']:checked").val() +"',{headers:false}) FROM ? ", [expotRows]);      
       $('#dltrend').html("Toogle button to download again.")  
+
+      $.get(window.location.href + "export_items_to_excel", { image : img.src }, function(data){
+        console.log(img.src)
+      })
+
     })
   },
   ChannelPerformanceRequest : function(){
