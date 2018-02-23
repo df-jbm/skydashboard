9c8c4676e7ba501569554a7343ae5631a3f0f02e
Jet Matias <jbm@datafactorycorp.com>
2018-02-23 17:00:32 +0800

    update js and controller

--- app/Http/Controllers/dbcontroller.php
+++ app/Http/Controllers/dbcontroller.php
@@ -86,7 +86,7 @@ class dbcontroller extends Controller
     }
     $trenddata .=
         "<tr>
-          <td colspan='10'><img src='". $r->image ."' height='200' width='800'></td>          
+          <td colspan='10'><img src='". $r->filename ."' height='200' width='800'></td>          
         </tr>
       </table>";
     echo $trenddata;        
