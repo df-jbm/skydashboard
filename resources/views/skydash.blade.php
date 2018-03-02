b4b95f9fc7ec48a290d71e5fc3fc6a15d3f97298
Jet Matias <jbm@datafactorycorp.com>
2018-03-02 13:54:46 +0800

    add library for jquery ui

--- resources/views/skydash.blade.php
+++ resources/views/skydash.blade.php
@@ -152,6 +152,7 @@
       </div>
     </main>
     <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootbox.min.js"></script>
