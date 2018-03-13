a8afc763c2e6c9bc11c70d627038aca8fd39c16d
jeromematias <jbm@datafactorycorp.com>
2018-03-13 16:26:43 +0800

    update

--- public/js/skydash.js
+++ public/js/skydash.js
@@ -658,7 +658,8 @@ var navmodule = {
 
       output += '</tr>'
       output += '</tbody></table>';
-      //$('#channelperformance').scrollTop(0)
+      $('#channelperformance').html('')
+      $('#channelperformance').scrollTop(0)
       $('#channelperformance').html(output)
       var $table = $('table#table-channelperformance');
       $table.floatThead();      
