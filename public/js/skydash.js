415d61852694a1bd44860b64376bbc58be8e7485
jeromematias <jbm@datafactorycorp.com>
2018-04-04 13:05:46 +0800

    update

--- public/js/skydash.js
+++ public/js/skydash.js
@@ -685,9 +685,11 @@ var navmodule = {
         height : "53px",
       })
       if(isIE11 == true){
-        $('#table-channelperformance tr td img').bicubicImgInterpolation({
-          crossOrigin: 'anonymous' //otherwise browser security error is triggered
-        });
+        $('#table-channelperformance tr td img').each(function(){
+          $(this).bicubicImgInterpolation({
+            crossOrigin: 'anonymous' //otherwise browser security error is triggered
+          });
+        })        
       }
       $('#table-channelperformance td:not(:first-child)').each(function(){
         $(this).click(function(){
