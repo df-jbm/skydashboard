a559f9d2cf730bd82a996dc786fa46e559ee2fb6
jeromematias <jbm@datafactorycorp.com>
2018-04-11 16:42:52 +0800

    update

--- resources/views/skydash.blade.php
+++ resources/views/skydash.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <!-- enable canvas for old versions of IE -->
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="edit-Type" edit="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/simple-line-icons.css" rel="stylesheet">
